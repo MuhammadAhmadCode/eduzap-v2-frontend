0a1f32b7bc8ef6305ea822eafc4d830f9aa700aa
MuhammadAhmadCode <ma2565983@gmail.com>
2026-02-26 21:38:55 +0500

    Started Implementing app with real backend

--- src/context/TaskContextProvider.jsx
+++ src/context/TaskContextProvider.jsx
@@ -3,7 +3,6 @@ import { TaskContext } from './TaskContext'
 
 
 const TaskContextProvider = ({ children }) => {
-
     const [tasks, setTasks] = useState(() => {
         const stored = localStorage.getItem("tasks")
         return stored ? JSON.parse(stored) : []
