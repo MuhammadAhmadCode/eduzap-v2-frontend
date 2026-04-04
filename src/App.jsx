c7e8e3f3f08f954fd681a579bfb14a1920a30d15
MuhammadAhmadCode <ma2565983@gmail.com>
2026-04-04 21:17:24 +0500

    ui problem fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import AppRoutes from './routes/AppRoutes'
+import { useContext } from "react";
+import AppRoutes from "./routes/AppRoutes";
+import AuthContext from "./context/AuthContext";
 
 const App = () => {
-
+  const { user } = useContext(AuthContext);
   return (
-    <div className='w-full pb-13 min-h-screen bg-slate-900'>
-      <AppRoutes/>
+    <div className={`${user ? "pb-13" : ""} w-full min-h-screen bg-slate-900`}>
+      <AppRoutes />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
