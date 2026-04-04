c4262eb12777c67eebc3e1d0c1808e9480789089
MuhammadAhmadCode <ma2565983@gmail.com>
2026-04-04 13:53:17 +0500

    fixed manual reload for getting new tasks

--- src/context/TaskContextProvider.jsx
+++ src/context/TaskContextProvider.jsx
@@ -1,31 +1,33 @@
-import { useState, useEffect, useContext } from 'react'
-import { TaskContext } from './TaskContext'
-import AuthContext from './AuthContext'
-import axios from 'axios'
-
+import { useState, useEffect, useContext } from "react";
+import { TaskContext } from "./TaskContext";
+import AuthContext from "./AuthContext";
+import axios from "axios";
 
 const TaskContextProvider = ({ children }) => {
-    const [tasks, setTasks] = useState([])
-    const {user} = useContext(AuthContext)
+  const [tasks, setTasks] = useState([]);
+  const { user } = useContext(AuthContext);
 
-    useEffect(() => {
-        if(!user) return
-        const fetchTasks = async()=>{
-            try {
-                const res = await axios.get("http://localhost:3000/api/tasks/alltasks",{withCredentials:true})
-                setTasks(res.data.tasks)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        fetchTasks()
-    }, [user,tasks])
+  useEffect(() => {
+    if (!user) return;
+    const fetchTasks = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:3000/api/tasks/alltasks",
+          { withCredentials: true },
+        );
+        setTasks(res.data.tasks);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTasks();
+  }, [user, tasks]);
 
-    return (
-        <TaskContext.Provider value={{tasks, setTasks}}>
-            {children}
-        </TaskContext.Provider>
-    )
-}
+  return (
+    <TaskContext.Provider value={{ tasks, setTasks }}>
+      {children}
+    </TaskContext.Provider>
+  );
+};
 
-export default TaskContextProvider
+export default TaskContextProvider;
