846dc3e765dfc98151d50514896cd5b99abd70be
MuhammadAhmadCode <ma2565983@gmail.com>
2026-04-04 21:25:24 +0500

    proper error handling for login done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,36 +3,40 @@ import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 
-
-
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate()
-  const {setUser } = useContext(AuthContext)
+  const [status, setStatus] = useState("");
+  const navigate = useNavigate();
+  const { setUser } = useContext(AuthContext);
 
-  const handleLogin = async(e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
     try {
       const res = await axios.post(
         "http://localhost:3000/api/auth/user/login",
-        { email,password},
-        { withCredentials: true }
-      )
-      setUser(res.data)
-      navigate("/")
+        { email, password },
+        { withCredentials: true },
+      );
+      setUser(res.data);
+      navigate("/");
     } catch (err) {
-      console.error(err)
+      console.error(err);
+      if (err.status === 400) {
+        setStatus("Invalid Email or Password!");
+      } else if (err.status === 500) {
+        setStatus("Internal Server Error, Try Again.");
+      } else {
+        setStatus("Something Went Wrong, Please try again later.");
+      }
     }
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-indigo-500 via-purple-500 to-pink-500">
       <div className="bg-gray-700 text-white shadow-lg rounded-xl p-8 max-w-md w-full">
-        <h2 className="text-2xl font-bold mb-6 text-center">
-          Welcome Back
-        </h2>
-
+        <h2 className="text-2xl font-bold mb-6 text-center">Welcome Back</h2>
+        <div className="text-red-400 text-lg text-center">{status}</div>
         <form onSubmit={handleLogin} className="space-y-5">
           <div>
             <label className="block mb-2" htmlFor="email">
@@ -74,7 +78,10 @@ const LoginPage = () => {
 
         <p className="mt-6 text-center">
           Don't have an account?{" "}
-          <Link to="/register" className="text-indigo-600 font-medium hover:underline">
+          <Link
+            to="/register"
+            className="text-indigo-600 font-medium hover:underline"
+          >
             Sign Up
           </Link>
         </p>
